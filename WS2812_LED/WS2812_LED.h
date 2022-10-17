6d685f2de0e27aa4847d2356e41bab8bcfd8cc33
Thea-E <84453030+Thea-E@users.noreply.github.com>
2022-10-17 01:14:14 -0400

    Add files via upload

---
+++ WS2812_LED/WS2812_LED.h
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _WS2812_H 
+#define _WS2812_H 
+ 
+void set_neopixel_color(uint time); 
+ 
+#endif
