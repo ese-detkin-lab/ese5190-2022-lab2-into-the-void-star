3cfe5bebce9dfe3f97271e99c8bdc5811a4d071f
WillChan9 <43904091+WillChan9@users.noreply.github.com>
2022-10-18 00:04:38 -0400

    Update ws2812.c

--- pio/ws2812/ws2812.c
+++ pio/ws2812/ws2812.c
@@ -75,34 +75,6 @@ const struct {
         {pattern_greys,   "Greys"},
 };
 
-// int main() {
-//     //set_sys_clock_48();
-//     stdio_init_all();
-//     printf("WS2812 Smoke Test, using pin %d", WS2812_PIN);
-//     gpio_init(NEOPWR);
-//     gpio_set_dir(NEOPWR, GPIO_OUT);
-//     gpio_put(NEOPWR, 1);
-//     // todo get free sm
-//     PIO pio = pio0;
-//     int sm = 0;
-//     uint offset = pio_add_program(pio, &ws2812_program);
-
-//     ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
-
-//     int t = 0;
-//     while (1) {
-//         int pat = rand() % count_of(pattern_table);
-//         int dir = (rand() >> 30) & 1 ? 1 : -1;
-//         puts(pattern_table[pat].name);
-//         puts(dir == 1 ? "(forward)" : "(backward)");
-//         for (int i = 0; i < 1000; ++i) {
-//             pattern_table[pat].pat(NUM_PIXELS, t);
-//             sleep_ms(10);
-//             t += dir;
-//         }
-//     }
-// }
-
 void ws2812_init(){
     // stdio_init_all();
     printf("WS2812 Smoke Test, using pin %d", WS2812_PIN);
